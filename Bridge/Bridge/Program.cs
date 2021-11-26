8111cacd1fce9180f471d70e7a40f248fdb7fe76
Jan Skwarczek <jasiu.skwarczek@gmail.com>
2021-11-26 09:21:28 +0100

    implemented bridge XML,CSV,FIXED date output

--- Bridge/Bridge/Program.cs
+++ Bridge/Bridge/Program.cs
@@ -1,12 +1,124 @@
 ﻿using System;
+using System.Threading.Channels;
 
-namespace Bridge
+namespace Program
 {
+    class DateFormat
+    {
+        protected IOutputFile _implementation;
+        
+        public DateFormat(IOutputFile implementation)
+        {
+            _implementation = implementation;
+        }
+
+        public virtual void Operation() {}
+    }
+    class XMLDateFormat : DateFormat
+    {
+        public XMLDateFormat(IOutputFile implementation) : base(implementation){}
+
+        public override void Operation()
+        {
+            base._implementation.writeXML();
+        }
+    }
+
+    class CSVDateFormat : DateFormat
+    {
+        public CSVDateFormat(IOutputFile implementation) : base(implementation){}
+        public override void Operation()
+        {
+            base._implementation.writeCSV();
+        }
+    }
+    
+    class FIXEDDateFormat : DateFormat
+    {
+        public FIXEDDateFormat(IOutputFile implementation) : base(implementation){}
+        public override void Operation()
+        {
+            base._implementation.writeFIXED();
+        }
+    }
+    public interface IOutputFile
+    {
+        void writeXML();
+        void writeCSV();
+        void writeFIXED();
+    }
+    class ConcreteImplementationA : IOutputFile
+    {
+        private string _y;
+        private string _m;
+        private string _d;
+        public ConcreteImplementationA(string y, string m, string d)
+        {
+            _y = y;
+            _m = m;
+            _d = d;
+        }
+        public void writeXML()
+        {
+            Console.WriteLine("<XML> {0}/{1}/{2}", _d, _m, _y);
+        }
+
+        public void writeCSV()
+        {
+            Console.WriteLine("<CSV> {0}/{1}/{2}", _d, _m, _y);
+        }
+
+        public void writeFIXED()
+        {
+            Console.WriteLine("<FIXED> {0}/{1}/{2}", _d, _m, _y);
+        }
+    }
+    class ConcreteImplementationB : IOutputFile
+    {
+        private string _y;
+        private string _m;
+        private string _d;
+        public ConcreteImplementationB(string y, string m, string d)
+        {
+            _y = y;
+            _m = m;
+            _d = d;
+        }
+        public void writeXML()
+        {
+            Console.WriteLine("<XML> {0}/{1}/{2}", _y, _m, _d);
+        }
+
+        public void writeCSV()
+        {
+            Console.WriteLine("<CSV> {0}/{1}/{2}", _y, _m, _d);
+        }
+
+        public void writeFIXED()
+        {
+            Console.WriteLine("<FIXED> {0}/{1}/{2}", _y, _m, _d);
+        }
+    }
+    class Client
+    {
+        public void ClientCode(DateFormat dateFormat)
+        {
+            dateFormat.Operation();
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Client client = new Client();
+
+            client.ClientCode(new XMLDateFormat(
+                new ConcreteImplementationB(
+                    "2021", "11", "26"
+                    )
+                )
+            );
         }
     }
 }
---
+++ Bridge/Bridge/README.md
@@ -0,0 +1,14 @@
+﻿# BRIDGE
+
+### W istniejącym systemie występują trzy różne rodzaje reprezentacji danych w pliku:
+- XML
+- CSV
+- FIXED length (załóżmy, że wszystkie pola maja długość 8).
+  
+
+  Zaimplementuj program, który umożliwi zapisywanie danych dotyczących kilku wybranych rodzajów
+  formatów dat (np. YYYYMMDD, MMDDYYYY, DDMMYY - nie bierzemy pod uwagę separatorów) dla trzech
+  powyższych rodzajów reprezentacji danych w pliku.
+  Na szczycie hierarchii klas powinny się znajdować
+  DateFormat (m.in. z metoda write())
+  OutputFile z metodami writeXML(), writeCSV(), writeFIXED()
